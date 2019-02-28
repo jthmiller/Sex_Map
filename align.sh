da2ebc0fd8b5292d3f173ef68ffc0fa79badc2df
jtmiller <jtmiller@umn.edu>
2019-02-27 20:39:09 -0600

    sdf

--- align.sh
+++ align.sh
@@ -1,25 +1,24 @@
 #!/bin/bash -l
 #SBATCH -J array_job
-#SBATCH -o /home/jmiller1/QTL_Map_Raw/QTL_scripts/out_er/array_job_out_%A_%a.txt
-#SBATCH -e /home/jmiller1/QTL_Map_Raw/QTL_scripts/out_er/array_job_err_%A_%a.txt
-#SBATCH --array=1-96
-#SBATCH -p hi
-#SBATCH --mem=30000
+#SBATCH -o /home/jmiller1/Sex_Map/er_out/array_job_out_%A_%a.txt
+#SBATCH -e /home/jmiller1/Sex_Map/er_out/array_job_err_%A_%a.txt
 #SBATCH --cpus-per-task=6
-###### number of nodes
+#SBATCH --mem-per-cpu=2gb
+#SBATCH -p lo
+#SBATCH --array=1-2
 
-###### number of processors
-####SBATCH -n 1
-
-indir=/home/jmiller1/QTL_Map_Raw/demulti_out/NBH/
 my_samtools=/home/jmiller1/bin/samtools-1.3/samtools
 my_bwa=/home/jmiller1/bin/bwa-0.7.12/bwa
 my_samblstr=/home/jmiller1/bin/samblaster-master/samblaster
+
+### NBH fastq
+indir=/home/jmiller1/QTL_Map_Raw/demulti_out/NBH/
 
 ## ncbi genome
 refdir=/home/jmiller1/bin/code/NCBI_Genome
 bwagenind=${refdir}/GCF_000826765.1_Fundulus_heteroclitus-3.0.2_genomic.fna
 
+## bamdir
 outdir=${basedir}/alignments
 
 lib=$(echo $fq1 | sed 's/_/\t/'|cut -f 1)
--- pipeline.sh
+++ pipeline.sh
@@ -1,9 +1,8 @@
 ### set base dir
 basedir=/home/jmiller1/sex_chrom
 
-
 ## align to ncbi genome
-sbatch -p low -c 24 -t 48:00:00 --mem=30000 --cpus-per-task=6 align.sh
+sbatch -p low -t 48:00:00 --export=basedir="/home/jmiller1/Sex_Map" ${basedir}/align.sh
 
 ## calculate depth to find rad sites
 sbatch -t 48:00:00 --mem=60000 depth.sh
