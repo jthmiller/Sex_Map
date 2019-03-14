bf7d613e9ec5526e22c0143c1fc9a7a59389b02c
jtmiller <jtmiller@umn.edu>
2019-03-14 12:40:44 -0400

    sdf

--- code/Plink_conv_rQTL.R
+++ code/Plink_conv_rQTL.R
@@ -3,8 +3,8 @@ library(qtl)
 source("/home/jmiller1/Sex_Map/code/PLINK2RQTL.f2.R")
 setwd("/home/jmiller1/Sex_Map")
 
-indir <- "/home/jmiller1/Sex_Map/plink_files"
-outdir <- "/home/jmiller1/Sex_Map/dataset"
+indir <- "/home/jmiller1/Sex_Map/plink_files/"
+outdir <- "/home/jmiller1/Sex_Map/dataset/"
 name <- "NBH_CHR5_filt_conv"
 
 ped <- paste(indir, name, ".ped", sep = "")
