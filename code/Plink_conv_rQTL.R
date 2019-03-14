418be8031dc677f76fffc4530f152efcbc161b3f
jtmiller <jtmiller@umn.edu>
2019-03-14 12:39:53 -0400

    sdf

--- code/Plink_conv_rQTL.R
+++ code/Plink_conv_rQTL.R
@@ -2,10 +2,11 @@
 library(qtl)
 source("/home/jmiller1/Sex_Map/code/PLINK2RQTL.f2.R")
 setwd("/home/jmiller1/Sex_Map")
-dir <- "/home/jmiller1/Sex_Map"
 
+indir <- "/home/jmiller1/Sex_Map/plink_files"
+outdir <- "/home/jmiller1/Sex_Map/dataset"
 name <- "NBH_CHR5_filt_conv"
 
-ped <- paste(dir, name, ".ped", sep = "")
-map <- paste(dir, name, ".map", sep = "")
-PLINKtoCSVR(ped = ped, map = map, out = paste(dir, name, ".parents.csvr", sep = ""))
+ped <- paste(indir, name, ".ped", sep = "")
+map <- paste(indir, name, ".map", sep = "")
+PLINKtoCSVR(ped = ped, map = map, out = paste(outdir, name, ".parents.csvr", sep = ""))
