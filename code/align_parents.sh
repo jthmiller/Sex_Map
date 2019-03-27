0ba3fd1b259ee759c072ed05fe2522c1bcc8f9bf
jtmiller <jtmiller@umn.edu>
2019-03-27 14:47:22 -0500

    ZX

--- code/align_parents.sh
+++ code/align_parents.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=2gb
 #SBATCH -p lo
-#SBATCH --array=39-42
+#SBATCH --array=1-2
 
 my_samtools=/home/jmiller1/bin/samtools-1.3/samtools
 my_bwa=/home/jmiller1/bin/bwa-0.7.12/bwa
